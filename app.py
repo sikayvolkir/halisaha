4a4b9515ae9db4d6314c274f29119e2db44ba482
sikayvolkir <141519015+sikayvolkir@users.noreply.github.com>
2026-09-05 16:16:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -374,7 +374,8 @@ with tab_emanet:
           stDiv.innerText = "QR Kodu Hizalayın...";
           requestAnimationFrame(scan);
         })
-        .catch(function(err) {stDiv.innerText = "Kamera Başlatılamadı!";
+        .catch(function(err) {
+          stDiv.innerText = "Kamera Başlatılamadı!";
         });
 
         function scan() {
